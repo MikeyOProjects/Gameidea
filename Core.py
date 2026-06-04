e307b824cbd5f4737e75e6da9683d2e2cb861b81
Mikey O <themiku13@gmail.com>
2026-06-04 15:10:41 -0500

    Add files via upload

---
+++ Core.py
@@ -0,0 +1,13 @@
+max_health = 100
+
+current_health = 100
+
+current_health <= max_health
+
+stab = 10
+
+attack = current_health - stab
+
+print("You have been stabbed! You lost " + str(stab) + " health points.")
+
+print(f"Your current health is {attack}")
