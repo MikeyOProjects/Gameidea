3f6ee89f18e02f50ea7110f8082f9b2cb294419f
Mikey O <themiku13@gmail.com>
2026-06-04 15:11:31 -0500

    update?

--- Core.py
+++ Core.py
@@ -11,3 +11,5 @@ attack = current_health - stab
 print("You have been stabbed! You lost " + str(stab) + " health points.")
 
 print(f"Your current health is {attack}")
+
+#why is this not comminting to git?
