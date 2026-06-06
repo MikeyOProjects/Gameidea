1204a67408e911752d4f2aabe131513c350def9e
Mikey O <themiku13@gmail.com>
2026-06-06 13:18:18 -0500

    added itemss

--- Core.py
+++ Core.py
@@ -6,10 +6,28 @@ current_health <= max_health
 
 stab = 10
 
-attack = current_health - stab
+current_health = current_health - stab
 
 print("You have been stabbed! You lost " + str(stab) + " health points.")
-
-print(f"Your current health is {attack}")
-
+print(f"Your current health is {current_health}")
 print("heal yourself.")
+
+potion = 25
+elixir = 50
+bandage = 10
+
+items = ["potion", "elixir", "bandage"]
+
+print("You have the following items in your inventory: " + ", ".join(items))
+print("Choose an item to heal yourself.")
+x = input()
+
+if x == "potion":
+    current_health += potion
+    print(f"You used a potion and healed {potion} health points. Your current health is {current_health}.")
+elif x == "elixir":
+    current_health += elixir
+    print(f"You used an elixir and healed {elixir} health points. Your current health is {current_health}.")
+elif x == "bandage":    
+    current_health += bandage
+    print(f"You used a bandage and healed {bandage} health points. Your current health is {current_health}.")
