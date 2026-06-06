4e173278b6a661805c515dbca2120b36fae4063f
Mikey O <themiku13@gmail.com>
2026-06-06 18:38:25 -0500

    current health fixed

--- Core.py
+++ Core.py
@@ -2,8 +2,6 @@ max_health = 100
 
 current_health = 100
 
-current_health <= max_health
-
 stab = 10
 
 current_health = current_health - stab
@@ -24,10 +22,18 @@ x = input()
 
 if x == "potion":
     current_health += potion
+    if current_health > max_health:
+        current_health = max_health
     print(f"You used a potion and healed {potion} health points. Your current health is {current_health}.")
 elif x == "elixir":
     current_health += elixir
+    if current_health > max_health:
+        current_health = max_health
     print(f"You used an elixir and healed {elixir} health points. Your current health is {current_health}.")
 elif x == "bandage":    
     current_health += bandage
+    if current_health > max_health:
+        current_health = maxHealth
     print(f"You used a bandage and healed {bandage} health points. Your current health is {current_health}.")
+else:
+    print("Invalid item. You cannot heal yourself.")
